72c51eeb214b3e5199b086a77371463915923ec4
leozaow <leandrobr9@gmail.com>
2024-03-13 17:33:45 -0300

    Create teste1.lua

---
+++ teste1.lua
@@ -0,0 +1,12 @@
+-- Atualiza e exibe a exp/hora e o tempo estimado para o próximo nível
+function atualizarExpHoraEPrevisao()
+    Client.showMessage("testando 1, 2 , 4 , 5")
+    
+end
+
+
+function iniciarTimer()
+    Timer("atualizarExpHoraEPrevisao", atualizarExpHoraEPrevisao, 3000) -- Atualiza a cada segundo
+end
+
+iniciarTimer()
