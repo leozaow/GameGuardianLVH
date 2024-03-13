2f3a917f41f6735646707d8d71257ae640c4e6d0
leozaow <leandrobr9@gmail.com>
2024-03-13 17:34:48 -0300

    Update teste1.lua

--- teste1.lua
+++ teste1.lua
@@ -1,6 +1,6 @@
 -- Atualiza e exibe a exp/hora e o tempo estimado para o próximo nível
 function atualizarExpHoraEPrevisao()
-    Client.showMessage("testando 1, 2 , 4 , 5")
+    Client.showMessage("testando 1, 2 , 4 , 5, 8")
     
 end
 
